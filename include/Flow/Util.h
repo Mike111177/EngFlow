d8f74978dc92c226dd9dcd719ad972ad621e8722
Mike Davidson <captain@mikeddavison.com>
2020-05-02 01:55:58 -0400

    Added Tracked_ptr

--- include/Flow/Util.h
+++ include/Flow/Util.h
@@ -34,4 +34,71 @@ namespace Flow{
 	size_t static_ptr<Owner, T, num>::count = 0;
 	template <typename Owner, typename T, size_t num>
 	std::unique_ptr<T> static_ptr<Owner, T, num>::member;
+
+
+
+	//tracked_pointer
+	//Like a shared ptr that can only have weak copies
+	template <typename T>
+	class utracked_ptr {
+		struct controlblock {
+			T* object = nullptr;
+			//Count = 0 Does not prevent destruction of object, think like a weak_ptr 
+			size_t count = 0; 
+			bool empty : 1 = true; //object is nullptr or uninitialized
+			bool destroyed : 1 = false; //utracked_ptr that created cb was destroyed
+		} * cb;
+		utracked_ptr(utracked_ptr<T>&) = delete;
+	public:
+		class tracker {
+			//Essentially a weak reference
+			controlblock* cb;
+			tracker(controlblock* cb) : cb(cb) { cb->count++; }
+			friend tracker utracked_ptr<T>::track();
+		public:
+			tracker(utracked_ptr<T> ptr) : cb(ptr.cb) { cb->count++; }
+			tracker(tracker& t) : cb(t.cb) { cb->count++; }
+			~tracker() {
+				cb->count--;
+				//If owner utracked_ptr destroyed, and no more trackers tracking it, destroy.
+				if (cb->destroyed && cb->count == 0) delete cb;
+			}
+			tracker track() { return tracker(cb); }
+			operator bool() { return !cb->destroyed && !cb->empty; }
+			T* operator->() { return cb->object; }
+			T* operator*() { return cb->object; }
+			T* get() { return cb->object; }
+		};
+		utracked_ptr() : cb(new controlblock) {}
+		utracked_ptr(T* ptr) : cb(new controlblock) {
+			cb->object = ptr;
+			cb->empty = false;
+		}
+		~utracked_ptr() {
+			//Destroy owned object
+			delete cb->object;
+			if (cb->count == 0) { //If no trackers tracking it, destroy.
+				delete cb;
+			} else { //Else mark as empty and destroyed so trackers know to destory when done.
+				cb->empty = true;
+				cb->destroyed = true;
+			}
+		}
+		tracker track() {return tracker(cb);}
+		explicit operator bool() {return !cb->destroyed && !cb->empty;}
+		T* operator->() { return cb->object; }
+		T* operator*() { return cb->object; }
+		T* get() { return cb->object; }
+		void swap(utracked_ptr<T>& ptr) {
+			auto t = ptr.get();
+			ptr.cb->object = cb->object;
+			cb->object = t;
+		}
+		
+
+	};
+	template<typename T, typename... Args>
+	utracked_ptr<T> make_utracked(Args... args) {
+		return new T(args...);
+	}
 }
